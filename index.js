5f5e3c62c8011c15300959fc41261d0fa11c4108
Kaivaan <cariaga.info@gmail.com>
2018-06-15 13:59:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
- // .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send("helow")
