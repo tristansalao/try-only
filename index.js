23d330ba0ae7c2c45b6060a87d213ab0c6cbbb25
Kaivaan <cariaga.info@gmail.com>
2018-06-15 15:33:55 +0800

    asf

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-
-  .get('/', (req, res) => res.render('index.html'))
+  .get('/', (req, res) => res.send('hello'))
+  .get('/test', (req, res) => res.render('index.html'))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
