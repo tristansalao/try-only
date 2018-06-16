cbda62e66ef0f47f393dcec928bc6433f4e95cad
Kaivaan <cariaga.info@gmail.com>
2018-06-16 08:54:25 +0800

    asf
    asf

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const history = require('connect-history-api-fallback')
-
+var serveStatic = require('serve-static')
 
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
 
 express()
+  
   .use(staticFileMiddleware)
   .use(history({
     disableDotRule: true,
@@ -17,6 +18,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.static(path.join(__dirname, 'src')))
   .use(express.static(path.join(__dirname, '/dist/')))
+  .use(serveStatic(path.join(__dirname, 'dist')))
   .set('views', path.join(__dirname, 'views'))
   .get('/', (req, res) => res.send('hello'))
   .get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html')))
