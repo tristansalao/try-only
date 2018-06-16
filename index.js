4e3626c12c3df452ac03931eaccd5609d6ef8d3f
Kaivaan <cariaga.info@gmail.com>
2018-06-16 11:42:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const PORT = process.env.PORT || 5000
 const history = require('connect-history-api-fallback')
 var serveStatic = require('serve-static')
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))//static location
 
 
 express()
   .get('/test2', (req, res) => res.send('hello'))
+
   .use(staticFileMiddleware)
   .use(history({
     disableDotRule: true,
@@ -17,12 +18,12 @@ express()
   .use(staticFileMiddleware)
 
 
-
+// 
  // .use(express.static(path.join(__dirname, 'public')))
  // .use(express.static(path.join(__dirname, 'src')))
   //.use(express.static(path.join(__dirname, '/dist/')))
  // .use(serveStatic(path.join(__dirname, 'dist')))
 //  .set('views', path.join(__dirname, 'views'))
 
- // .get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html'))) // not working after adding vue
+ // .get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html'))) 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
