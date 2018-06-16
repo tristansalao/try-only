cc0eab4f1d7031beacf55aa9bfcc730858dc2270
Kaivaan <cariaga.info@gmail.com>
2018-06-16 08:44:58 +0800

    asf

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
 
 express()
-  //.use(staticFileMiddleware)
-  //.use(history({
-  //  disableDotRule: true,
-  //  verbose: true
-  //}))
-  //.use(staticFileMiddleware)
+  .use(staticFileMiddleware)
+  .use(history({
+    disableDotRule: true,
+    verbose: true
+  }))
+  .use(staticFileMiddleware)
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.static(path.join(__dirname, 'src')))
   .use(express.static(path.join(__dirname, '/dist/')))
   .set('views', path.join(__dirname, 'views'))
   .get('/', (req, res) => res.send('hello'))
-  //.get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html')))
+  .get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html')))
   
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
