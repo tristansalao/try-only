2f77a375fb7bbc623c44b7da3bba1adfb0928c79
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-06-18 17:03:27 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
 	<v-data-table dark
       :headers="headers"
-      :items="desserts"
+      :items="data"
       hide-actions
-      class="elevation-1: pt-3"
+      class="elevation-1: pt-2"
 
     >
       <template slot="items" slot-scope="props">
@@ -23,108 +23,43 @@ export default {
     return {
     	headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'No.',
           align: 'left',
           sortable: false,
-          value: 'name'
+          value: 'no'
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
+        { text: 'HO ID', value: 'hoid' },
+        { text: 'Commission', value: 'commission' },
+        { text: 'Deposit', value: 'deposit' },
+        { text: 'Deposit(Transfer)', value: 'deptransfer' },
+        { text: 'Withdraw', value: 'withdraw' },
+        { text: 'Withdraw(Transfer)', value: 'withtransfer' },
+        { text: 'Betting Amount', value: 'betamount' },
+        { text: 'Rake', value: 'rake' },
+        { text: 'User Holding Money', value: 'userholding' },
+        { text: 'Operating Head Office Profit', value: 'offprofit' },
+        { text: 'Head Office Money', value: 'offmoney' },
+        { text: 'Money of Low Rank', value: 'moneylowrank' },
+        { text: 'Low Rank', value: 'lowrank' },
       ],
-      desserts: [
+      data: [
         {
           value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
+          no: 'Frozen Yogurt',
+          hoid: 159,
+          commission: 6.0,
+          deposit: 24,
+          deptransfer: 4.0,
+          withdraw: '1%',
+          withtransfer: '1%',
+          betamount: '1%',
+          rake: '1%',
+          userholding: '1%',
+          offprofit: '1%',
+          offmoney: '1%',
+          moneylowrank: '1%',
+          lowrank: '1%',
         },
-        {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        }
       ]    	
     }
   }
